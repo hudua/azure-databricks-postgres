87da2bfdc9f328389665b8b1ca8754e642903869
Hubert Duan <hudua@microsoft.com>
2019-04-11 14:46:13 -0700

    updated connection string

--- postgresql_connection.scala
+++ postgresql_connection.scala
@@ -1,4 +1,4 @@
-val jdbcUrl = s"jdbc:postgresql://<name>.postgres.database.azure.com:5432/example?user=...@...&password=...&stringtype=unspecified&ssl=true&ssl_ca_certs=<path>/ca-certificate.crt"
+val jdbcUrl = s"jdbc:postgresql://<servicename>.postgres.database.azure.com:5432/example?user=<user>@<servicename>&password=<pw>&stringtype=unspecified&ssl=true&ssl_ca_certs=<path>/ca-certificate.crt"
 val connectionProperties = new java.util.Properties()
 val driverClass = "org.postgresql.Driver"
 connectionProperties.setProperty("Driver", driverClass)
